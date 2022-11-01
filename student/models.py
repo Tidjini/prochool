c5ef0dbfefd33c2c5c265b9d188f1190cfb91cbe
Tidjini <tidjini.messaoudi@gmail.com>
2022-11-01 10:16:21 +0100

    Check with Student Code Barre Model

--- student/models.py
+++ student/models.py
@@ -1,3 +1,13 @@
+import black
 from django.db import models
 
 # Create your models here.
+
+
+class Student(models.Model):
+    """Student Model
+
+    pk: is a code barre with EAN13
+    """
+
+    code_barre = models.CharField(max_length=13, primary=True)
