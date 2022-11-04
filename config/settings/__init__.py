120bdf288b75465bcd30eea30b20667b0b23658e
Tidjini <tidjini.messaoudi@gmail.com>
2022-11-04 08:36:04 +0100

    Switch to my custom dev settings (custom cache time)

--- config/settings/__init__.py
+++ config/settings/__init__.py
@@ -3,4 +3,4 @@ Just remember, for specific:
     # python manage.py shell --settings=prochool.settings.dev <or whatever setting>
     # python manage.py runserver --settings=prochool.settings.dev ... 
 '''
-from .dev import *
+from .dev_tidjini import *
